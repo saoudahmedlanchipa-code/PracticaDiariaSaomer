0e5d0cf52c8f0834ea6d9d10ee4f63921a248dba
Saoud Ahmed <saoud.ahmed.lanchipa@alumnos.uta.cl>
2025-11-08 15:58:47 -0300

    Aejer 3 y 4
    ejercicios 3 y 4

---
+++ Aejer3.py
@@ -0,0 +1,12 @@
+"""
+Escriba un algoritmo que solicite dos números y calcule la expresión: x*3 + y*15
+"""
+
+
+def Algoritmo(n1,n2):
+    return n1*3 + n2*15
+
+num1 = int(input("Ingrese primer valor: "))
+num2 = int(input("Ingrese segundo valor: "))
+
+print(Algoritmo(num1,num2))
---
+++ Aejer4.py
@@ -0,0 +1,11 @@
+"""
+4. Escriba un algoritmo que calcule el valor de “y” en función de “x”, según la función: y(x) = 3x + 2.
+
+"""
+
+def Funcion(n1):
+    return n1*3 + 2
+
+num1 = int(input("Ingresa X: "))
+
+print(Funcion(num1))
