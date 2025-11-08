8d55e9918d9ca6a4445e2790fc4f5c4ea9378b62
Saoud Ahmed <saoud.ahmed.lanchipa@alumnos.uta.cl>
2025-11-08 14:30:55 -0300

    Ejercicio1 de la primera guia dada en python

---
+++ Aejer1.py
@@ -0,0 +1,12 @@
+
+def Promedio(num1,num2, num3, num4):
+    return (num1+num2+num3+num4)/4
+
+
+
+num1 = int(input("Ingrese un numero :"))
+num2 = int(input("Ingrese un numero :"))
+num3 = int(input("Ingrese un numero :"))
+num4 = int(input("Ingrese un numero :"))
+
+print("El promedio es :" + Promedio(num1,num2,num3,num4))
